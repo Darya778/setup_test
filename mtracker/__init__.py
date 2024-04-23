e7fa1c1809d45145cfdfc6cbda4d2cdb529aaea4
Darysha <112837727+Darya778@users.noreply.github.com>
2024-04-23 09:38:05 +0800

    Update __init__.py

--- mtracker/__init__.py
+++ mtracker/__init__.py
@@ -1,19 +1,23 @@
-def f(x: int):
-    if x != -1:
-        x += 1
-    return x
-def sapper(arr: list, mini: list):
-    for x, y in mini:
-        arr[x][y] = -1
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                arr[x+i][y+j] = f(arr[x+i][y+j])
-    return arr
-n, m, k = map(int, input('Введите размер поля и кол-во мин (x y n): ').split())
-n += 2
-m += 2
-mini = [[int(input()) for c in range(2)] for k in range(k)]
-arr = [[0 for j in range(m)] for i in range(n)]
-arr = sapper(arr, mini)
-for i in range(1, n-1):
-    print(*arr[i][1:m-1])
+print('0 в качестве знака операции'
+      '\nзавершит работу программы\n')
+
+while True:
+    s = input('Знак (+, -, *, /): ')
+    if s == '0':
+        break
+    if s in ('+', '-', '*', '/'):
+        a = float(input('a = '))
+        b = float(input('b = '))
+        if s == '+':
+            print('%.2f' % (a + b))
+        elif s == '-':
+            print('%.2f' % (a - b))
+        elif s == '*':
+            print('%.2f' % (a * b))
+        elif s == '/':
+            if b != 0:
+                print('%.2f' % (a / b))
+            else:
+                print('Деление на ноль!')
+    else:
+        print('Неверный знак операции!')
