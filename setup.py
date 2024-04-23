44fc4df4984e40e38f8870990d7399c19c139b87
Darysha <112837727+Darya778@users.noreply.github.com>
2024-04-23 10:38:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
    author_email='darya.n.2004@mail.com',
    url='https://github.com/Darya778/setup_test',
    packages=['mtracker'], 
-   install_requires=[numpy==1.26.4, setuptools==59.6.0],
+   install_requires=['numpy==1.26.4', 'setuptools==59.6.0'],
    extras_require={
         'test': [
             'pytest',
