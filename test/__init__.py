bc79774c60cd959f7b4205946316553739fa8ac1
Darysha <112837727+Darya778@users.noreply.github.com>
2024-04-23 09:58:12 +0800

    Update __init__.py

--- test/__init__.py
+++ test/__init__.py
@@ -1,43 +1,15 @@
-from io import StringIO
-import sys
 import unittest
 
-def calculate_expression(s, a, b):
-    result = None
-    if s == '+':
-        result = a + b
-    elif s == '-':
-        result = a - b
-    elif s == '*':
-        result = a * b
-    elif s == '/':
-        if b != 0:
-            result = a / b
-        else:
-            return 'Деление на ноль!'
-    return result
+class TestAddNumbers(unittest.TestCase):
 
-class TestCalculator(unittest.TestCase):
+    def test_add_positive_numbers(self):
+        self.assertEqual(add_numbers(3, 5), 8)
 
-    def setUp(self):
-        self.held_output = StringIO()
-        sys.stdout = self.held_output
+    def test_add_negative_numbers(self):
+        self.assertEqual(add_numbers(-3, -5), -8)
 
-    def tearDown(self):
-        sys.stdout = sys.__stdout__
-
-    def test_addition(self):
-        self.assertEqual(calculate_expression('+', 5, 3), 8)
-
-    def test_subtraction(self):
-        self.assertEqual(calculate_expression('-', 5, 3), 2)
-
-    def test_multiplication(self):
-        self.assertEqual(calculate_expression('*', 5, 3), 15)
-
-    def test_division(self):
-        self.assertEqual(calculate_expression('/', 6, 3), 2)
-        self.assertEqual(calculate_expression('/', 5, 0), 'Деление на ноль!')
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add_numbers(3, -5), -2)
 
 if __name__ == '__main__':
     unittest.main()
